b2b182fba8b00a222ff522a8470f6c1a7ae74c53
rozcua <91014905+rozcua@users.noreply.github.com>
2024-06-08 15:35:33 +0200

    poprawki w mainpage i loggedIn
    żeby stylizacja działała

---
+++ src/components/LoggedIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoggedIn = () => {
+    return (
+        <div className='loggedIn'>
+            <h1>Witaj ponownie użytkowniku!</h1>
+            <p>Możesz teraz w odpowiednich zakładkach dodać książki do: <b>chcę przeczytać</b>, <b>mojej półki</b> lub do <b>ulubionych</b>.</p>
+            <p>Szukasz konkretnej książki? Skorzystaj z naszej wyszukiwarki!</p>
+        </div>
+    );
+};
+
+export default LoggedIn;
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,1 +1,17 @@
 import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div className='mainContainer'>
+            <h1>Witamy w Book App - miejsca, gdzie znajdziesz wszystkie ulubione książki!</h1>
+            <div className='siteDesc'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti beatae impedit similique iure magni neque veniam aliquid, ullam vero. Aliquid, repudiandae? Quam, minima doloribus eveniet, libero aperiam quisquam perspiciatis aliquid at dolorem dicta qui sequi alias eos officiis omnis, consequatur error! Rem labore unde est voluptas dolores dolore corporis iure. Dolore reiciendis impedit non, quibusdam cupiditate error omnis reprehenderit possimus consequuntur labore quo maxime doloribus deserunt laboriosam quidem tenetur nesciunt quas, in quod eveniet iure. Esse voluptatem nulla, autem quis eos nemo iste. Perferendis consequuntur ad soluta aliquam possimus quibusdam, voluptatibus corrupti alias natus non, beatae facere iste iure nostrum.
+            </p></div>
+            <div className='cite'>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque deleniti doloribus quis quidem? Sunt, magni!"</p></div>
+            <div className='author'><p> - Paolo Coelho</p></div>
+        </div>
+    );
+};
+
+export default MainPage;
