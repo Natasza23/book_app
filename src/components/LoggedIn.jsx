0e0d664c56d9ddbecc40fbf438037d366da0bf89
Natasza23 <nat.szu24@gmail.com>
2024-06-08 14:52:17 +0200

    poprawki kosmetyczne
    poprawione wyświetlanie książek w sekcjach po zalogowaniu, dodany tekst na stronę główną

--- src/components/LoggedIn.jsx
+++ src/components/LoggedIn.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 const LoggedIn = () => {
     return (
-        <div>
+        <div className='loggedIn'>
             <h1>Witaj ponownie użytkowniku!</h1>
             <h2>Możesz teraz w odpowiednich zakładkach dodać książki do: przeczytanych, ulubionych lub do przeczytania.</h2>
-            {/* Dodaj tutaj więcej informacji o użytkowniku */}
         </div>
     );
 };
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const MainPage = () => {
     return (
-        <div>
-            <h1>Lorem ipsum</h1>
-            <h2>Ipsum Lorem</h2>
+        <div className='mainPage'>
+            <h1>Witamy na aplikacji książkowej!</h1>
+            <h2>W tej aplikacji możesz przeglądać i wyszukiwać książki, a po zarejestrowaniu i zalogowaniu możesz również dodawać je do sekcji do przeczytania i przeczytanych. Kiedy już dodasz książki do swojej półki, możesz również dodać je do ulubionych.</h2>
         </div>
     );
 };
