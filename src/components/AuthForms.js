dca892944262fa9ec079342ecb49d2d22fe0029d
Natasza23 <nat.szu24@gmail.com>
2024-06-08 13:09:32 +0200

    poprawki 08.06 netlify

--- src/components/AuthForms.js
+++ src/components/AuthForms.js
@@ -123,7 +123,7 @@ const AuthForms = () => {
                 <i className="uil uil-times form_close"></i>
                 <div className="form login_form">
                     <form action="#">
-                        <h2>Login</h2>
+                        <h2>Zaloguj się</h2>
                         <div className="input_box">
                             <input type="email" placeholder="Wpisz swój email" id="email" required />
                             <i className="uil uil-envelope-alt email"></i>
@@ -146,7 +146,7 @@ const AuthForms = () => {
                 </div>
                 <div className="form signup_form">
                     <form action="#">
-                        <h2>Signup</h2>
+                        <h2>Zarejestruj się</h2>
                         <div className="input_box">
                             <input type="email" placeholder="Wpisz swój email" id="email" required />
                             <i className="uil uil-envelope-alt email"></i>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,22 +11,23 @@ const Navbar = () => {
     return (
         <>
             <header className="header">
+            <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
                 <div className="wrapper">
                     <div className="navbar">
                         <img src={logo} className="nav_logo" alt="logo" />
                         <div className="navMenu">
                             <ul>
-                                <li><Link to="/MainPage"><h3>Strona główna</h3></Link></li>
+                                <li><Link to="/MainPage">Strona główna</Link></li>
                                 {user && (
                                     <>
-                                        <li><Link to="/TBR"><h3>Chcę przeczytać</h3></Link></li>
-                                        <li><Link to="/MojaPolka"><h3>Moja półka</h3></Link></li>
-                                        <li><Link to="/Favorites"><h3>Ulubione</h3></Link></li>
+                                        <li><Link to="/TBR">Chcę przeczytać</Link></li>
+                                        <li><Link to="/MojaPolka">Moja półka</Link></li>
+                                        <li><Link to="/Favorites">Ulubione</Link></li>
                                     </>
                                 )}
-                                <li><Link to="/katalog"><h3>Katalog</h3></Link></li>
-                                <li><Link to="/Najlepsze"><h3>Najlepiej oceniane</h3></Link></li>
-                                <li><Link to="/SearchBox"><h3>Szukaj</h3></Link></li>
+                                <li><Link to="/katalog">Katalog</Link></li>
+                                <li><Link to="/Najlepsze">Najlepsze</Link></li>
+                                <li><Link to="/SearchBox">Szukaj</Link></li>
                             </ul>
                             {!user ? (
                                 <button className="button" id="form-open">Zaloguj</button>
